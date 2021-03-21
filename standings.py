8a0e0dc5c812f9b5e4e0aa2d0000b118002d1287
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2021-03-21 13:08:19 +0530

    league table for major tournaments

--- standings.py
+++ standings.py
@@ -3,13 +3,21 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 
 
-def get_table():
+def get_table(league):
     header = {
         "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
     }
 
+    league = league.lower()
+    if league in ['laliga', 'la liga']:
+        league = 'la-liga'
+    if league in ['serie-a', 'serie a']:
+        league = 'serie-a-calcio'
+    if league == 'efl':
+        league = 'efl-championship'
+
     page = requests.get(
-        "https://www.sportskeeda.com/go/epl/standings", headers=header)
+        f"https://www.sportskeeda.com/go/{league}/standings", headers=header)
     soup = BeautifulSoup(page.content, 'lxml')
     table = soup.find('table')
     rows = table.find_all('tr')
